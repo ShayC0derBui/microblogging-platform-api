1e6089e1c87c3cb50fcc9f74ea6a22c6017a5668
ShayC0derBui <sinhaprem636@gmail.com>
2024-05-11 19:48:31 +0530

    feat(posts): controllers

--- src/posts/controllers/post-controller.controller.ts
+++ src/posts/controllers/post-controller.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { PostServiceService } from '../services/post-service.service';
 
-@Controller('post-controller')
-export class PostControllerController {}
+@Controller('posts')
+export class PostControllerController {
+  // - Get all posts made by a user
+  // - Get all posts existing posts
+  // - Get all followers of a user
+  // - Get user’s basic details
+  // - Update user’s details
+  // - Create a post
+
+  constructor(private readonly postService: PostServiceService) { }
+
+  // Get all posts made by a user
+  @Get(':userId')
+  async getPostsByUser(@Param('userId') userId: string) {
+    return this.postService.getPostsByUser(userId);
+  }
+
+  // Get all existing posts
+  @Get('posts')
+  async getAllPosts() {
+    return this.postService.getAllPosts();
+  }
+
+  // Create a post
+  @Post()
+  async createPost(@Body('userId') userId: string, @Body('body') body: string) {
+    return this.postService.createPost(userId, body);
+  }
+}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostServiceService } from './services/post-service.service';
 import { PostControllerController } from './controllers/post-controller.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [PostServiceService],
-  controllers: [PostControllerController]
+  controllers: [PostControllerController],
 })
-export class PostsModule {}
+export class PostsModule { }
